361d56b3a557d4848ea5d6e5841023badd778b61
ferreiraklet <90654646+ferreiraklet@users.noreply.github.com>
2021-10-12 18:28:29 -0300

    Update ytpy.py

--- ytpy.py
+++ ytpy.py
@@ -71,7 +71,7 @@ def video(video):
 #print("Title:  ", youtube.title)
 
 def downloading_video():
-    print(f"""{a_ex}
+    print(f"""{c_ex}
  _____                    _                 _ _
 (____ \                  | |               | (_)
  _   \ \ ___  _ _ _ ____ | | ___   ____  _ | |_ ____   ____
